31ec46fc1b77153a06c2b2749deae3ab822d5161
fiosanabria <87140079+fiosanabria@users.noreply.github.com>
2021-10-20 16:49:17 -0300

    nuevo

---
+++ Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Main.css"
+
+const Main = () => {
+    return (
+        <main>
+            <h1 className="H1">Haikus</h1>
+        </main>
+    )
+}
+
+export default Main
